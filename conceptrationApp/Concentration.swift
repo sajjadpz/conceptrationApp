fc710659b6e5dd8e84f90cb75491455c42e1ac32
sajjadpz <sajjad.pervaiz@hotmail.com>
2019-12-20 13:11:41 +0500

    use of closure and extension of protocols, lecture4

--- conceptrationApp/Concentration.swift
+++ conceptrationApp/Concentration.swift
@@ -15,17 +15,18 @@ struct Concentration {
     //computed property
     private var indexOfOneAndOnlyFaceUp: Int? {
         get {
-            var foundIndex: Int?
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil {
-                        foundIndex = index
-                    } else {
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
+            return cards.indices.filter{cards[$0].isFaceUp}.oneAndOnly //use of closure
+//            var foundIndex: Int?
+//            for index in cards.indices {
+//                if cards[index].isFaceUp {
+//                    if foundIndex == nil {
+//                        foundIndex = index
+//                    } else {
+//                        return nil
+//                    }
+//                }
+//            }
+//            return foundIndex
         }
         set {
             for index in cards.indices {
@@ -56,5 +57,12 @@ struct Concentration {
             let card = Card()
             cards += [card, card]//copies the card strcut to matching card
         }
+    }
+}
+
+//extension of protocols
+extension Collection {
+    var oneAndOnly: Element? {
+        return count == 1 ? first: nil
     }
 }
